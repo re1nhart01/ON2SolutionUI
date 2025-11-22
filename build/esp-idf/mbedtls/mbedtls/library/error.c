02484415cb9ad34259ec60f8a72c49edbe4d01ed
eugene.kokaiko@gmail.com <eugene.kokaiko@gmail.com>
2025-11-23 00:01:44 +0200

    Ignore build directory

--- build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/evgeniy/esp-idf/components/mbedtls/mbedtls/library/error.c
---
+++ components/foundation/core/navigation/stack_navigator/navigation_screen_base.h
@@ -0,0 +1,8 @@
+//
+// Created by evgeniy on 11/22/25.
+//
+
+#ifndef NAVIGATION_SCREEN_BASE_H
+#define NAVIGATION_SCREEN_BASE_H
+
+#endif //NAVIGATION_SCREEN_BASE_H
